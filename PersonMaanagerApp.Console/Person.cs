c7de5028d3563fd9c41a643756f338ba99c29ff4
yury <shubin1978@omnicommdv.ru>
2023-11-28 19:52:01 +0800

    Project finish

--- PersonMaanagerApp.Console/Person.cs
+++ PersonMaanagerApp.Console/Person.cs
@@ -2,5 +2,21 @@
 
 public class Person
 {
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string FullName => $"{FirstName} {LastName}";
     
+    public DateTime DateOfBirth { get; set; }
+
+    public int Age
+    {
+        get
+        {
+            var today = DateTime.Today;
+            var age = today.Year - DateOfBirth.Year;
+            if (DateOfBirth.Date > today.AddYears(-age)) age--;
+            return age;
+        }
+    }
 }
--- PersonMaanagerApp.Console/Program.cs
+++ PersonMaanagerApp.Console/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Data;
+using Microsoft.Data.Sqlite;
+using PersonMaanagerApp.Console;
+
+const string CONNECTION_STRING = @"Data Source=D:\Programming\C#\8-DataBase\SQLite_Test\PersonMaanagerApp\SQL\persons.db;";
+var db = new SqliteConnection(CONNECTION_STRING);
+db.Open();
+
+var sql = "SELECT * FROM table_persons";
+var command = new SqliteCommand(commandText: sql, connection: db);
+var result = command.ExecuteReader();
+var persons = new List<Person>();
+if (result.HasRows)
+{
+    while (result.Read())
+    {
+        persons.Add(new Person
+        {
+            Id = result.GetInt32("id"),
+            FirstName = result.GetString(1),
+            LastName = result.GetString(2),
+            DateOfBirth = result.GetDateTime(3)
+        }); 
+    }
+    
+}
+db.Close();
+
+foreach (var p in persons)
+{
+    Console.WriteLine($"{p.Id}: {p.FullName}, {p.Age}");
+}
